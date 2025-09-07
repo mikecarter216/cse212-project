d8443b868c88fa72d0d7a06cfa7c789d5dccc816
Michael Akpan <michaelakpan216@gmail.com>
2025-09-07 16:39:55 +0100

    ..

--- week05/code/Maze.cs
+++ week05/code/Maze.cs
@@ -76,4 +76,9 @@ public class Maze
         // Otherwise, we are good
         return true;
     }
+
+    internal bool IsValidMove(int nx, int ny, List<(int, int)> currPath)
+    {
+        throw new NotImplementedException();
+    }
 }
